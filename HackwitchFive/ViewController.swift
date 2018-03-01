a75884543c41507c6eeb36d9c91445d076074ef1
calvior <calvior@hawaii.edu>
2018-02-28 18:58:28 -1000

    Completed HackwitchFive.

--- HackwitchFive/ViewController.swift
+++ HackwitchFive/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerThree: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var myFriendsArray: [String] = ["Mahina", "Adrianne", "Raena"]
+    
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+        func numberOfSelections(in tableView: UITableView) -> Int {
+            return 1;
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+   
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)  -> Int {
+        return myFriendsArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+            let text = myFriendsArray[indexPath.row]
+            cell.textLabel?.text=text
+            return cell
+    }
 }
-
