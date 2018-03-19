78e77a5ea9f66404498c1e3052accff0ea15f40d
CM Student <cmstudent@UHWO-ACM-08.local>
2018-03-19 10:26:28 -1000

    hackwitch 7. part 9 complete

--- HackwitchFive/ViewController.swift
+++ HackwitchFive/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewControllerThree: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
     var myFriendsArray: [String] = ["Mahina", "Adrianne", "Raena"]
+   
+    var restaurantImageData = [String]()
     
     @IBOutlet var tableView: UITableView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
@@ -20,8 +25,15 @@ class ViewControllerThree: UIViewController, UITableViewDataSource, UITableViewD
         func numberOfSelections(in tableView: UITableView) -> Int {
             return 1;
         }
+       
+        //Part 6
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        
+        restaurantImageData = dict!.object(forKey: "restaurantImages") as! [String]
     }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,10 +43,22 @@ class ViewControllerThree: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)  -> Int {
         return myFriendsArray.count
     }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
             let text = myFriendsArray[indexPath.row]
             cell.textLabel?.text=text
             return cell
     }
+    
+   
+    //Part 7
+    func tableView(_tableView:UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    
+    
+    
+    
 }
